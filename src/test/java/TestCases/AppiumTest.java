cac195633c1d66b45116a89d4e3b51220516a620
malathirumalaivelu <malathirumalaivelu@gmail.com>
2019-04-14 16:31:03 +0530

    first commit

---
+++ src/test/java/TestCases/AppiumTest.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+	@Test
+	public void postJira() {
+		System.out.println(" postJira");
+	}
+	@Test
+	public void deleteTwitter() {
+		System.out.println("deleteTwitter");
+	}
+}
---
+++ src/test/java/TestCases/RestApiTest.java
@@ -0,0 +1,14 @@
+package TestCases;
+
+import org.testng.annotations.Test;
+
+public class RestApiTest {
+	@Test
+	public void nativeAndroid() {
+		System.out.println("nativeAndroid");
+	}
+	@Test
+	public void iosApp() {
+		System.out.println("iosApp");
+	}
+}
