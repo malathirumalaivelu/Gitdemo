899a6ae3b0bd1b1c8e048d401f662fe1487d05bd
malathirumalaivelu <malathirumalaivelu@gmail.com>
2019-04-14 17:44:50 +0530

    this is commit from Asiantimezone

--- src/test/java/TestCases/AppiumTest.java
+++ src/test/java/TestCases/AppiumTest.java
@@ -6,6 +6,8 @@ public class AppiumTest {
 	@Test
 	public void postJira() {
 		System.out.println(" postJira");
+		System.out.println(" postJira2"); 
+		System.out.println(" postJira3");
 	}
 	@Test
 	public void deleteTwitter() {
